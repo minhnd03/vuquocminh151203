587e2f4c1a8e82d62b12b404d493fe0e54a18fc3
minhnd03 <vuminhnd03@gmail.com>
2025-05-24 16:54:42 +0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VuQuocMinh_2121051527_KiemTra_2425.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
